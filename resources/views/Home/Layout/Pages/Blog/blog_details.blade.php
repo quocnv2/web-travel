bd73d1dcaf6f01ce35210ca5a957785c49c72c3a
DESKTOP-QD7JEVF\ADMIN <garena.hp02@gmail.com>
2024-05-14 10:09:19 +0700

    sửa phần bình luận

--- resources/views/Home/Layout/Pages/Blog/blog_details.blade.php
+++ resources/views/Home/Layout/Pages/Blog/blog_details.blade.php
@@ -2,7 +2,7 @@
 @php
     use Carbon\Carbon;
 
-    $slugCategory = isset($objCategory) ? $objCategory : ''
+    $slugCategory = isset($objCategory) ? $objCategory : '';
 @endphp
 @section('view')
     <section class="page-header">
@@ -14,10 +14,10 @@
             <div class="page-header__breadcrumb-box">
                 <ul class="trevlo-breadcrumb">
                     <li><a href="{{ route('home') }}">Trang Chủ</a></li>
-                    @if($slugCategory)
+                    @if ($slugCategory)
                         <li><a href="{{ route('listBlog') }}">Bài Viết</a></li>
                         <li><a href="{{ route('detailBlog', $slugCategory->slug) }}">Danh Mục</a></li>
-                        <li>{{$slugCategory->name}}</li>
+                        <li>{{ $slugCategory->name }}</li>
                     @else
                         <li><a href="{{ route('listBlog') }}">Bài Viết</a></li>
                     @endif
@@ -35,7 +35,7 @@
                     <div class="sidebar-blog sidebar-blog--left">
                         <aside class="widget-area">
                             <div class="sidebar-blog__single sidebar-blog__single--posts wow animated fadeInUp"
-                                 data-wow-delay="0.1s" data-wow-duration="1500ms">
+                                data-wow-delay="0.1s" data-wow-duration="1500ms">
                                 <h4 class="sidebar-blog__title">Bài Viết Mới Nhất</h4><!-- /.sidebar-blog__title -->
                                 <ul class="sidebar-blog__posts ">
                                     @foreach ($blog_new as $valNew)
@@ -49,7 +49,7 @@
                                                     {{ Carbon::parse($valNew->timeCreate)->locale('vi')->isoFormat('Do [tháng] M [năm] YYYY') }}
                                                 </p><!-- /.sidebar__posts-date -->
                                                 <h4 class="sidebar-blog__posts-title"><a
-                                                        href="{{route('detailBlog', $valNew->slug)}}">{{ $valNew->name }}</a>
+                                                        href="{{ route('detailBlog', $valNew->slug) }}">{{ $valNew->name }}</a>
                                                 </h4>
                                                 <!-- /.sidebar-blog__posts-title -->
                                             </div><!-- /.sidebar-blog__posts-content -->
@@ -58,7 +58,7 @@
                                 </ul><!-- /.sidebar-blog__posts  -->
                             </div><!-- /.sidebar-blog__single -->
                             <div class="sidebar-blog__single sidebar-blog__single--categories wow animated fadeInUp"
-                                 data-wow-delay="0.2s" data-wow-duration="1500ms">
+                                data-wow-delay="0.2s" data-wow-duration="1500ms">
                                 <h4 class="sidebar-blog__title">Danh Mục</h4><!-- /.sidebar-blog__title -->
                                 <ul class="sidebar-blog__categories ">
                                     @foreach ($categories as $cat)
@@ -82,9 +82,9 @@
                         <div class="blog-card-three">
                             <div class="blog__card">
                                 <div class="blog__card-content wow animated fadeInUp" data-wow-delay="0.1s"
-                                     data-wow-duration="1500ms">
-                                    <h3 class="blog__card-title">{{$objBlog->name}}</h3>
-                                    <p class="blog__card-text">{!!$objBlog->info_details_blog!!}</p>
+                                    data-wow-duration="1500ms">
+                                    <h3 class="blog__card-title">{{ $objBlog->name }}</h3>
+                                    <p class="blog__card-text">{!! $objBlog->info_details_blog !!}</p>
 
                                 </div><!-- /.blog-details__card-content -->
                             </div><!-- /.blog-details__card -->
@@ -95,18 +95,17 @@
                                 <div class="post-category__btn-group">
                                     <div class="tour-listing-five__card__btn-group">
                                         @php
-                                            $imageArray= json_decode($objBlog->imageArray, true);
-                                            $videoArray= json_decode($objBlog->videoArray, true);
+                                            $imageArray = json_decode($objBlog->imageArray, true);
+                                            $videoArray = json_decode($objBlog->videoArray, true);
                                         @endphp
                                         <a href="javascript:void(0);"
-                                           class="tour-listing-five__card__popup-btn tour-listing-five__card__popup-btn--camera trevlo-image-popup"
-                                           data-gallery-options='{
+                                            class="tour-listing-five__card__popup-btn tour-listing-five__card__popup-btn--camera trevlo-image-popup"
+                                            data-gallery-options='{
                                                             "items": [
-                                                                   @foreach($imageArray as $index => $imgs)
-                                                                        @if(isset($imgs['link']) && $imgs['link'] != '')
-                                                                             { "src": "{{ $imgs['link'] }}" }@if(!$loop->last),@endif
-                                                                        @endif
-                                                                    @endforeach
+                                                                   @foreach ($imageArray as $index => $imgs)
+                                                                        @if (isset($imgs['link']) && $imgs['link'] != '')
+                                                                             { "src": "{{ $imgs['link'] }}" }@if (!$loop->last),@endif
+                                                                        @endif @endforeach
                                                             ],
                                                             "gallery": {
                                                               "enabled": true
@@ -116,14 +115,13 @@
                                             <span class="icon-photo-camera-1"></span>
                                         </a>
                                         <a href="javascript:void(0);"
-                                           class="tour-listing-five__card__popup-btn trevlo-image-popup"
-                                           data-gallery-options='{
+                                            class="tour-listing-five__card__popup-btn trevlo-image-popup"
+                                            data-gallery-options='{
                                                             "items": [
-                                                                   @foreach($videoArray as $index => $videos)
-                                                                        @if(isset($videos['link']) && $videos['link'] != '')
-                                                                            {"src": "{{ $videos['link'] }}", "style": "width: 100%;"}@if(!$loop->last),@endif
-                                                                        @endif
-                                                                    @endforeach
+                                                                   @foreach ($videoArray as $index => $videos)
+                                                                        @if (isset($videos['link']) && $videos['link'] != '')
+                                                                            {"src": "{{ $videos['link'] }}", "style": "width: 100%;"}@if (!$loop->last),@endif
+                                                                        @endif @endforeach
                                                             ],
                                                             "gallery": {
                                                               "enabled": true
@@ -138,65 +136,65 @@
 
                         </div><!-- /.post-info -->
                     </div><!-- /.blog-details -->
+                    @if ($listCommentBlog && count($listCommentBlog) > 0)
+                        <div class="comment-wrapper">
+                            <div class="comment-wrapper__title-box">
+                                <h3 class="comment-wrapper__title">Bình Luận</h3>
+                            </div><!-- /.comment-wrapper__title-box -->
+                            <div class="comment-box comment-box-one">
+                                @foreach ($listCommentBlog as $valueCommentBlog)
+                                    <div class="comment-box__image wow animated fadeInUp" data-wow-delay="0s"
+                                        data-wow-duration="1500ms">
+                                        <img src="{{ $valueCommentBlog->objBlog->imgBanner }}" alt="David Shon">
+                                    </div><!-- /.comment-box__image -->
+                                    <div class="comment-box__content wow animated fadeInUp" data-wow-delay="0.3s"
+                                        data-wow-duration="1500ms">
+                                        <h3 class="comment-box__name">{{ $valueCommentBlog->name }}</h3>
+                                        <p class="comment-box__text">{{ $valueCommentBlog->commentUser }}</p>
+                                    </div><!-- /.comment-box__content -->
+                                @endforeach
+                            </div><!-- /.comment-box -->
 
-                    <div class="comment-wrapper">
-                        <div class="comment-wrapper__title-box">
-                            <h3 class="comment-wrapper__title">Bình Luận</h3>
-                        </div><!-- /.comment-wrapper__title-box -->
-                        <div class="comment-box comment-box-one">
-                            @foreach($listCommentBlog as $valueCommentBlog)
-                                <div class="comment-box__image wow animated fadeInUp" data-wow-delay="0s"
-                                     data-wow-duration="1500ms">
-                                    <img src="{{$valueCommentBlog->objBlog->imgBanner}}" alt="David Shon">
-                                </div><!-- /.comment-box__image -->
-                                <div class="comment-box__content wow animated fadeInUp" data-wow-delay="0.3s"
-                                     data-wow-duration="1500ms">
-                                    <h3 class="comment-box__name">{{$valueCommentBlog-> name}}</h3>
-                                    <p class="comment-box__text">{{$valueCommentBlog->commentUser}}</p>
-                                </div><!-- /.comment-box__content -->
-                            @endforeach
-                        </div><!-- /.comment-box -->
-
-                    </div><!-- /.comment-wrapper -->
+                        </div><!-- /.comment-wrapper -->
+                    @endif
                     <div class="comment-form">
                         <div class="comment-form__inner-container container-fluid">
                             <h3 class="comment-form__title">Để Lại Bình Luận Của Bạn</h3>
                             <form class="form-one row gutter-20" method="POST"
-                                  action="{{ route('create_comment_blog',['slug' => $objBlog->slug]) }}">
+                                action="{{ route('create_comment_blog', ['slug' => $objBlog->slug]) }}">
                                 @csrf
-                                <div class="col-md-6 wow animated fadeInUp" data-wow-delay="0s"
-                                     data-wow-duration="1500ms">
+                                <div class="col-md-6 wow animated fadeInUp" data-wow-delay="0s" data-wow-duration="1500ms">
                                     <div class="form-one__group">
                                         <input type="text" name="name" id="form-one-name-input"
-                                               placeholder="Tên Của Bạn" class="form-one__input"
-                                               value="{{ old('name') }}">
+                                            placeholder="Tên Của Bạn" class="form-one__input" value="{{ old('name') }}">
                                         @error('name')
-                                        <small style="color: #f33923;">{{ $message }}</small>
+                                            <small style="color: #f33923;">{{ $message }}</small>
                                         @enderror
                                     </div><!-- /.form-one__group -->
                                 </div><!-- /.col-md-6 -->
                                 <div class="col-md-6 wow animated fadeInUp" data-wow-delay="0.3s"
-                                     data-wow-duration="1500ms">
+                                    data-wow-duration="1500ms">
                                     <div class="form-one__group">
-                                        <input type="email" name="email" id="form-one-email-input"
-                                               placeholder="Email" class="form-one__input" value="{{ old('email') }}">
+                                        <input type="email" name="email" id="form-one-email-input" placeholder="Email"
+                                            class="form-one__input" value="{{ old('email') }}">
                                     </div><!-- /.form-one__group -->
                                 </div><!-- /.col-md-6 -->
                                 <div class="col-12 wow animated fadeInUp" data-wow-delay="0.1s"
-                                     data-wow-duration="1500ms">
+                                    data-wow-duration="1500ms">
                                     <div class="form-one__group">
-                                        <textarea name="commentUser" id="form-one-message-input" cols="30"
-                                                  rows="10" placeholder="Comment Của Bạn"
-                                                  class="form-one__message form-one__input">{{ old('commentUser') ?? 'Nội Dung Bài Viết' }}</textarea>
+                                        <textarea name="commentUser" id="form-one-message-input" cols="30" rows="10"
+                                            placeholder="Comment Của Bạn" class="form-one__message form-one__input">{{ old('commentUser') ?? 'Nội Dung Bài Viết' }}</textarea>
                                     </div><!-- /.form-one__group -->
                                 </div><!-- /.col-12-->
                                 <input type="hidden" name="idBlog" value="{{ $objBlog->id }}">
-                                <input type="hidden" name="status" value="{{ $objBlog->status??0 }}">
-                                <input type="hidden" name="commentAdmin" value="{{ $objBlog->commentAdmin??"Admin" }}">
+                                <input type="hidden" name="status" value="{{ $objBlog->status ?? 0 }}">
+                                <input type="hidden" name="commentAdmin"
+                                    value="{{ $objBlog->commentAdmin ?? 'Admin' }}">
                                 <div class="col-12 wow animated fadeInUp" data-wow-delay="0.2s"
-                                     data-wow-duration="1500ms">
+                                    data-wow-duration="1500ms">
                                     <div class="form-one__btn-box">
-                                        <button type="submit" class="form-one__btn trevlo-btn trevlo-btn--base"><span>Gửi</span>
+                                        <button type="submit"
+                                            class="form-one__btn trevlo-btn trevlo-btn--base"><span>Gửi</span>
                                         </button>
                                     </div><!-- /.form-one__btn-box -->
                                 </div><!-- /.col-12-->
