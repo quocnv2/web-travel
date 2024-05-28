d43161fc9516576286dcd050c50023b83e6a6fff
DESKTOP-QD7JEVF\ADMIN <garena.hp02@gmail.com>
2024-05-28 08:58:28 +0700

    GIAO DIỆN HEDER

--- resources/views/Home/Layout/Body/header.blade.php
+++ resources/views/Home/Layout/Body/header.blade.php
@@ -2,49 +2,49 @@
     <div class="container">
         <div class="main-header__inner">
             <div class="main-header__logo">
-                <a href="{{route('home')}}">
-                    <img src="{{ url('assets') }}/trevlo/images/logo-light-2.png" alt="Trevlo HTML"
-                         width="187" class="main-header__logo__1">
-                    <img src="{{ url('assets') }}/trevlo/images/logo-light-4.png" alt="Trevlo HTML"
-                         width="187" class="main-header__logo__2">
+                <a href="{{ route('home') }}">
+                    <img src="{{ url('assets') }}/trevlo/images/logo-light-2.png" alt="Trevlo HTML" width="187"
+                        class="main-header__logo__1">
+                    <img src="{{ url('assets') }}/trevlo/images/logo-light-4.png" alt="Trevlo HTML" width="187"
+                        class="main-header__logo__2">
                 </a>
             </div><!-- /.main-header__logo -->
             <div class="main-header__right">
                 <nav class="main-header__nav main-menu">
                     <ul class="main-menu__list">
                         <li>
-                            <a href="{{route('home')}}">Trang chủ</a>
+                            <a href="{{ route('home') }}">Trang chủ</a>
                         </li>
-                        <li class="dropdown ">
-                            <span>Danh Mục</span>
+                        <li class="dropdown">
+                            <a href="#">Danh Mục</a>
                             <ul class="sub-menu">
                                 @foreach ($categories as $category)
                                     <li>
-                                        <a href="{{ route('listTour_Category', $category->slug) }}">{{ $category->name }}</a>
-                                        {{--                                                <a href="{{ url('/' . $category->slug) }}">{{ $category->name }}</a>--}}
+                                        <a
+                                            href="{{ route('listTour_Category', $category->slug) }}">{{ $category->name }}</a>
+                                        {{--                                                <a href="{{ url('/' . $category->slug) }}">{{ $category->name }}</a> --}}
                                     </li>
                                 @endforeach
                             </ul>
                         </li>
-
                         <li>
-                            <a href="{{route('listTour')}}">Tour</a>
+                            <a href="{{ route('listTour') }}">Tour</a>
                         </li>
                         <li>
-                            <a href="{{route('listRoom')}}">Phòng</a>
+                            <a href="{{ route('listRoom') }}">Phòng</a>
                         </li>
                         <li>
-                            <a href="{{route('listBlog')}}">Bài viết</a>
+                            <a href="{{ route('listBlog') }}">Bài viết</a>
                         </li>
                         <li>
                             <a href="https://www.facebook.com/profile.php?id=100069226627675"
-                               target="black">Facebook</a>
+                                target="black">Facebook</a>
                         </li>
                         {{-- <li>
                             <a href="">Youtube</a>
                         </li> --}}
                         <li>
-                            <a href="{{route('contact_web')}}">Liên hệ</a>
+                            <a href="{{ route('contact_web') }}">Liên hệ</a>
                         </li>
                     </ul>
                 </nav><!-- /.main-header__nav -->
@@ -57,8 +57,8 @@
                     <i class="trevlo-one-icon-search" aria-hidden="true"></i>
                     <span class="sr-only">Search</span>
                 </a><!-- /.search-toggler -->
-                <a href="{{route("register_counsel")}}"
-                   class="main-header__button trevlo-btn trevlo-btn--two trevlo-btn--base">
+                <a href="{{ route('register_counsel') }}"
+                    class="main-header__button trevlo-btn trevlo-btn--two trevlo-btn--base">
                     <span>Đăng Ký Tư Vấn</span>
                     <i class="trevlo-one-icon-up-right-arrow"></i>
                 </a>
