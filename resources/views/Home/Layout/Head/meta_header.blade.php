643c627740c3bec9ff0ac6812002f2d31e3d0ac4
TienDat <garena.hp02@gmail.com>
2024-06-05 21:00:20 +0700

    code mới

--- resources/views/Home/Layout/Head/meta_header.blade.php
+++ resources/views/Home/Layout/Head/meta_header.blade.php
@@ -1,3 +1,9 @@
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-<meta name="description" content="Trevlo is a modern HTML Template for Beauty, Spa Centers, Hair, Nail, Spa Salons & Cosmetic shops. The template perfectly fits Beauty Spa, Salon, and Wellness Treatments websites and businesses." />
+<meta name="description"
+    content="Khám phá vùng Độc Lạ tây Bắc với chúng tôi - công ty du lịch cam kết đảm bảo giá tốt nhất và mang lại sự hài lòng cao nhất từ khách hàng.." />
+<meta property="og:url" content="{{ url()->current() }}" />
+<meta property="og:type" content="website" />
+<meta property="og:title" content="Sông Đà Tour - Độc Lạ Tây Bắc" />
+<meta property="og:image"
+    content="https://res.cloudinary.com/dtvczrcdk/image/upload/v1716540133/hxg0i7isak52a4urswim.jpg" />
